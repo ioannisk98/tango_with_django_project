34919ffa2f23da2ee9a4c937347ed3daf5b68d0c
Ioannis Kyriacou <2462278k@student.gla.ac.uk>
2022-01-22 23:55:14 +0000

    Chapter 3

---
+++ rango/urls.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 22 20:15:41 2022
+
+@author: user
+"""
+
+from django.urls import path
+from rango import views
+
+app_name='rango'
+
+urlpatterns=[
+    path('',views.index,name='index'),
+    path('about/',views.about,name='about')
+    ]
