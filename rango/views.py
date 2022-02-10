187abdc3123c39a6c7c28f0683291232ecf658e5
Ioannis Kyriacou <2462278k@student.gla.ac.uk>
2022-02-10 10:39:12 +0000

    Capter 9

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,8 @@ from rango.forms import PageForm
 from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 
 
@@ -50,7 +52,7 @@ def show_category(request, category_name_slug):
         
     return render(request, 'rango/category.html', context=context_dict)
 
-
+@login_required
 def add_category(request):
     form=CategoryForm()
     
@@ -60,13 +62,13 @@ def add_category(request):
         if form.is_valid():
             form.save(commit=True)
             
-            return redirect('/rango/')
+            return redirect("/rango/")
         else:
             print(form.errors)
             
     return render(request, 'rango/add_category.html', {'form': form})
 
-
+@login_required
 def add_page(request, category_name_slug):
     try:
         category= Category.objects.get(slug=category_name_slug)
@@ -148,5 +150,15 @@ def user_login(request):
         
     else:
         return render(request,'rango/login.html')
+    
+    
+@login_required
+def restricted(request):
+    return render(request, 'rango/restricted.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
 
 
